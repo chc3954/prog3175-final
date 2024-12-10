4152de99a27f3f2e14a15aad8b71bf110ea2c750
Hyunchul Cho <chc3954@gmail.com>
2024-12-09 19:42:44 -0500

    Added Read endpoints (all and by id) for Person resource

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ app.post("/person", (req, res) => {
   res.status(201).json(newPerson);
 });
 
+// get all people
+app.get("/person/all", (req, res) => {
+  // load the data from the file
+  const data = loadData();
+  // send the data as a response
+  res.json(data);
+});
+
+// get a person by id
+app.get("/person/:id", (req, res) => {
+  // load the data from the file
+  const data = loadData();
+  // find the person with the given id
+  const person = data.find((person) => person.id === parseInt(req.params.id));
+  // send the person as a response
+  res.json(person);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
